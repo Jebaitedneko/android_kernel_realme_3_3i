862a5998c277c69716f52d5b5ac94f23c0c45582
Jebaitedneko <16777012+Jebaitedneko@users.noreply.github.com>
2020-06-30 16:24:34 +0530

    use correct defconfig

--- gcc_build.sh
+++ gcc_build.sh
@@ -4,8 +4,8 @@
 [ -d "los-4.9-64" ] && echo "ARM64 TC Present." || echo "ARM64 TC Not Present. Downloading..." | git clone --depth=1 https://github.com/LineageOS/android_prebuilts_gcc_linux-x86_aarch64_aarch64-linux-android-4.9 los-4.9-64
 [ -d "los-4.9-32" ] && echo "ARM32 TC Present." || echo "ARM32 TC Not Present. Downloading..." | git clone --depth=1 https://github.com/LineageOS/android_prebuilts_gcc_linux-x86_arm_arm-linux-androideabi-4.9 los-4.9-32
 
-# make O=out ARCH=arm64 realme_defconfig
-make O=out ARCH=arm64 k71v1_64_bsp_defconfig
+make O=out ARCH=arm64 realme_defconfig
+# make O=out ARCH=arm64 k71v1_64_bsp_defconfig
 
 PATH="$(pwd)/los-4.9-64/bin:$(pwd)/los-4.9-32/bin:${PATH}" \
 make                  O=out \
